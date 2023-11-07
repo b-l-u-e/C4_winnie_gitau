6a68ec1e7e63a6cf4614e2ce9ab6099fe011cd03
b-l-u-e <winnie.gitau282@gmail.com>
2023-11-07 21:33:02 +0300

    first react app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css'
+
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <p>I am Winnie Gitau, this is my first React application.</p>
+    
+    </div>
+  )
+}
+
+export default App
