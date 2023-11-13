5ae6580b66d09bf61c5413173135322c2ebd6a14
b-l-u-e <winnie.gitau282@gmail.com>
2023-11-13 08:52:40 +0300

    add button

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 
 import './App.css'
 import UserPortfolio from './components/UserPortfolio'
+import Button from './components/button/Button'
 
 function App() {
   return (
     <div>
       <h1>Hello world</h1>
       <UserPortfolio />
+      <Button label="Login" />
+      <Button label="Sign in" />
     
     </div>
   )
